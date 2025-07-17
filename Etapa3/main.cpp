47b9aeb89a3fc7f6c8a3093279fdc56a25837905
Lucasmath07 <lucas.mat.ferr.roch@gmail.com>
2025-07-17 16:44:38 -0300

    Update main.cpp

--- Etapa3/main.cpp
+++ Etapa3/main.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+#include "MapaAltitudes.h"
+using namespace std;
 
+int main() {
+    int N = 3;           // Define matriz 9x9
+    double rug = 0.5;    // Fator de rugosidade
+
+    MapaAltitudes mapa(N, rug);
+    mapa.gerarTerreno();
+
+    cout << "Mapa de Altitudes Gerado:\n";
+    mapa.imprimir();
+
+    mapa.salvarEmArquivo("altitudes.txt");
+
+    // Testar leitura
+    MapaAltitudes novoMapa(1, rug);
+    novoMapa.carregarDeArquivo("altitudes.txt");
+    cout << "\nMapa lido do arquivo:\n";
+    novoMapa.imprimir();
+
+    return 0;
+}
